218206b733bd54fb4b0379cc30ab129010fcc694
ym123 <1070038355@qq.com>
2018-11-12 14:12:21 +0800

    redux以前的知识点更新包含热更新，别名等。

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router , Route, Switch,Link} from 'react-router-dom'
-import Home from '../pages/Home/Home';
-import Page1 from '../pages/Page1/Page1';
+import Home from 'pages/Home/Home';
+import Page1 from 'pages/Page1/Page1';
 
 
 const getRouter = () =>(
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 module.exports = {
     //入口
-    entry:path.join(__dirname,'src/index.js'),
+    entry:[
+        'react-hot-loader/patch',
+        path.join(__dirname,'src/index.js')
+    ],
     //出口------输出到dist文件夹，输出文件名字为bundle.js
     output:{
         path:path.join(__dirname,'./dist'),
@@ -17,5 +20,18 @@ module.exports = {
                 include:path.join(__dirname,'src')
             }
         ]
+    },
+    devServer:{
+        port:8080,
+        contentBase: path.join(__dirname,'./dist')
+        ,historyApiFallback:true
+        ,host:'0.0.0.0'
+    },
+    resolve:{
+        alias:{
+            pages:path.join(__dirname,'src/pages'),
+            component:path.join(__dirname,'src/component'),
+            router:path.join(__dirname,'src/router')
+        }
     }
 };
