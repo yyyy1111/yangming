b3f7d44e38181ab7960b1c90482fd1a48bc18df7
ym123 <1070038355@qq.com>
2018-11-12 18:07:43 +0800

    action的相关知识

---
+++ src/redux/actions/userInfo.js
@@ -0,0 +1,17 @@
+export const GET_USER_INFO_REQUEST = "userInfo/GET_USER_INFO_REQUEST";
+export const GET_USER_INFO_SUCCESS = "userInfo/GET_USER_INFO_SUCCESS";
+export const GET_USER_INFO_FAIL = "userInfo/GET_USER_INFO_FAIL";
+
+function getUserInfoRequest(){
+    return {
+        type : GET_USER_INFO_REQUEST
+    }
+}
+
+function getUserInfoSuccess(){
+
+}
+
+function getUserInfoFail(){
+
+}
