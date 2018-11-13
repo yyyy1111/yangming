42454eb049eeee91471e88878cebb23b513bf161
ym123 <1070038355@qq.com>
2018-11-13 10:51:26 +0800

    redux的combineReducers优化

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,12 +1,19 @@
+import {combineReducers} from "redux";
+
 import counter from 'reducers/counter';
 import userInfo from 'reducers/userInfo';
 
 
 
 //组合的reducers
-export default function combineReducers (state = {}, action){
-    return {
-        counter : counter(state.counter, action),
-        userInfo : userInfo(state.userInfo,action)
-    }
-}
+// export default function combineReducers (state = {}, action){
+//     return {
+//         counter : counter(state.counter, action),
+//         userInfo : userInfo(state.userInfo,action)
+//     }
+// }
+
+export default combineReducers({
+    counter,
+    userInfo
+});
