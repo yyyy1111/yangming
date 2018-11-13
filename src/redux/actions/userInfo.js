34d708e656fa07789a3e01905ce92a4a67acbee2
ym123 <1070038355@qq.com>
2018-11-13 10:08:37 +0800

    与redux相关的中间件插件redux-thunk以及所有redux相关配置到此结束

--- src/redux/actions/userInfo.js
+++ src/redux/actions/userInfo.js
@@ -2,16 +2,46 @@ export const GET_USER_INFO_REQUEST = "userInfo/GET_USER_INFO_REQUEST";
 export const GET_USER_INFO_SUCCESS = "userInfo/GET_USER_INFO_SUCCESS";
 export const GET_USER_INFO_FAIL = "userInfo/GET_USER_INFO_FAIL";
 
+
+//返回actions的函数们
 function getUserInfoRequest(){
     return {
         type : GET_USER_INFO_REQUEST
     }
 }
 
-function getUserInfoSuccess(){
-
+function getUserInfoSuccess(userInfo){
+    return {
+       type :  GET_USER_INFO_SUCCESS,
+       userInfo: userInfo
+    }
 }
 
 function getUserInfoFail(){
+    return {
+        type : GET_USER_INFO_FAIL
+    }
+}
 
+
+//用dispatch把函数派发出去
+
+export function getUserInfo(){
+    return function (){
+        //发送异步请求
+        dispatchEvent(getUserInfoRequest());
+
+        return fetch('http://localhost:8080/api/user.json')
+            .then((response => {
+                return response.json()
+            }))
+            .then((json) => {
+                    dispatch(getUserInfoSuccess(json))
+                }
+            ).catch(
+                () => {
+                    dispatch(getUserInfoFail());
+                }
+            )
+    }
 }
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,12 @@
 import counter from 'reducers/counter';
+import userInfo from 'reducers/userInfo';
 
+
+
+//组合的reducers
 export default function combineReducers (state = {}, action){
     return {
-        counter : counter(state.counter, action)
+        counter : counter(state.counter, action),
+        userInfo : userInfo(state.userInfo,action)
     }
 }
