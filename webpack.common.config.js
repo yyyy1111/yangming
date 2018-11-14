4e6c0e76e8bd773082afdea7f3070b46a0ab0183
ym123 <1070038355@qq.com>
2018-11-14 11:51:51 +0800

    合并提取webpack公共配置

---
+++ webpack.common.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+commonConfig = {
+    entry:{
+        app:[
+            path.join(__dirname,'src/index.js')
+        ],
+        vendor:['react','react-router-dom','redux','react-dom','react-redux']
+    }
+    ,output: {
+        path:path.join(__dirname,'./dist'),
+        filename:'[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        publicPath: '/',
+    },
+    module: {
+        rules:[
+            {   
+                // 编译js
+                test:/\.js$/,
+                use:['babel-loader?cacheDirectory=true'],
+                include:path.join(__dirname,'src')
+            }
+            ,{
+                //编译图片
+                test:/\.(png|jpg|gif)$/i,
+                use: [{
+                    loader:'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }]
+            }
+            ,{
+                // 编译less
+                test:/\.less$/,
+                use:[{
+                    loader:"style-loader"
+                },{
+                    loader:"css-loader"
+                },{
+                    loader:"less-loader"
+                }]
+            }
+        ]
+    }
+    ,plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname,'src/index.html')
+        })
+        
+    ]
+    ,resolve:{
+        //别名配置
+        alias:{
+            pages:path.join(__dirname,'src/pages'),
+            component:path.join(__dirname,'src/component'),
+            router:path.join(__dirname,'src/router'),
+            actions:path.join(__dirname,'src/redux/actions'),
+            reducers:path.join(__dirname,'src/redux/reducers'),
+            // redux:path.join(__dirname,'src/redux')
+        }
+    }
+    ,optimization: {
+        splitChunks: {
+            cacheGroups: {
+               commons: {
+                    chunks:"initial",
+                    minChunks: 2, maxInitialRequests: 5,
+                    minSize:0
+               },
+               vendor: {
+                   test:/node_modules/,
+                   chunks:'initial',
+                   name:'vendor',
+                   priority:10,
+                   enforce:true
+               }
+               
+            }
+        }
+    }
+};
+module.exports = commonConfig ;
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,56 +1,30 @@
+const merge = require('webpack-merge');
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
-module.exports = {
+
+const commonConfig = require('./webpack.common.config.js');
+
+const devConfig = {
+    //devtool优化
+    devtool: 'inline-source-map',
     //入口
     entry:{
        app:[
             'react-hot-loader/patch',
             path.join(__dirname,'src/index.js')
-        ],
-        vendor:['react','react-router-dom','redux','react-dom','react-redux']
+        ]
     },
-    //出口------输出到dist文件夹，输出文件名字为bundle.js
+    //出口------
     output:{
-        path:path.join(__dirname,'./dist'),
-        filename:'[name].[hash].js',
-        chunkFilename: '[name].[chunkhash].js'
+        filename:'[name].[hash].js'
     },
     //babel解析
     /*cacheDirectory是用来缓存编译结果，下次编译加速*/
     module:{
         rules:[
-            {   
-                // 编译js
-                test:/\.js$/,
-                use:['babel-loader?cacheDirectory=true'],
-                include:path.join(__dirname,'src')
-            }
-            ,{
+            {
                 // 编译css
                 test:/\.css$/,
                 use: ['style-loader','css-loader']
-            }
-            ,{
-                // 编译less
-                test:/\.less$/,
-                use:[{
-                    loader:"style-loader"
-                },{
-                    loader:"css-loader"
-                },{
-                    loader:"less-loader"
-                }]
-            }
-            ,{
-                //编译图片
-                test:/\.(png|jpg|gif)$/i,
-                use: [{
-                    loader:'url-loader',
-                    options: {
-                        limit: 8192
-                    }
-                }]
             }
         ]
     },
@@ -60,47 +34,16 @@ module.exports = {
         contentBase: path.join(__dirname,'./dist')
         ,historyApiFallback:true
         ,host:'0.0.0.0'
-    },
-    resolve:{
-        //别名配置
-        alias:{
-            pages:path.join(__dirname,'src/pages'),
-            component:path.join(__dirname,'src/component'),
-            router:path.join(__dirname,'src/router'),
-            actions:path.join(__dirname,'src/redux/actions'),
-            reducers:path.join(__dirname,'src/redux/reducers'),
-            // redux:path.join(__dirname,'src/redux')
-        }
-    },
-    //devtool优化
-    devtool: 'inline-source-map',
-    //插件
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.join(__dirname,'src/index.html')
-        })
-        // ,new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'vendor'
-        // })
-    ],
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-               commons: {
-                    chunks:"initial",
-                    minChunks: 2, maxInitialRequests: 5,
-                    minSize:0
-               },
-               vendor: {
-                   test:/node_modules/,
-                   chunks:'initial',
-                   name:'vendor',
-                   priority:10,
-                   enforce:true
-               }
-               
-            }
-        }
     }
 };
+
+module.exports = merge({
+    customizeArray(a,b,key) {
+        /*entry.app不合并，全替换*/
+        if(key === 'entry.app'){
+            return b;
+        }
+        return undefined;
+    }
+})(commonConfig,devConfig);
+
